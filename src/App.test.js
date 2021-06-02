2e25d57b4fb64eff2ae4172026a41653236c7e92
Mark Venema <markavenema@gmail.com>
2021-06-02 11:35:37 +0900

    Initial Commit

---
+++ src/App.test.js
@@ -0,0 +1,25 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('Renders without errors', () => {
+
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter display starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
+});
